98b9abeb2976de8335d8fde336d43e74f5f28231
Masa Kakiya <digitalanimator2000@gmail.com>
2020-10-06 10:57:14 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Masa Kakiya on 2020/10/06.
+//  Copyright © 2020 Masa.Kakiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
